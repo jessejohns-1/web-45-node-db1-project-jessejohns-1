f0cb1f7098d6b41ce1d6b424bd0047d8ecb78a55
Jesse <eoshout@Gmail.com>
2021-09-06 16:16:27 -0400

    mvp

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,29 +1,29 @@
 const Account = require("./accounts-model");
-const db = ('../../data/db-config.js')
+const db = require('../../data/db-config.js')
 
 exports.checkAccountPayload = (req, res, next) => {
-  const errorMessage = {status:400} 
-  const { name, budget} = req.body
+  const errorMessage = {status: 400}
+  const {name, budget} = req.body;
   if(name === undefined || budget === undefined){
-    errorMessage.message = 'name and budget are required'
-  }else if(typeof name !== "string"){
-    errorMessage.message = 'name of account must be a string'
-  }else if (name.trim().length < 3 || name.trim().length > 100 ){
-    errorMessage.message = 'name of account must be between 3 and 100'
+      errorMessage.message = "name and budget are required";
   }
-  else if(typeof budget !== 'number' || isNaN(budget) ){
-    errorMessage.message = 'budget of account must be a number'
+  else if(typeof name !== "string"){
+      errorMessage.message = "name of account must be a string";
   }
-  else if (budget < 0 || budget > 1000000){
-    errorMessage.message = "budget of account is too large or too small"
+  else if(name.trim().length < 3 || name.trim().length > 100){
+      errorMessage.message = "name of account must be between 3 and 100";
   }
-    if (errorMessage.message){
-      next(errorMessage)
-
-  }else{
-    next()
+  else if(typeof budget !== "number" || isNaN(budget)){
+      errorMessage.message = "budget of account must be a number";
   }
-
+  else if(budget < 0 || budget > 1000000){
+      errorMessage.message = "budget is too large or too small";
+  }
+  if (errorMessage.message){ next(errorMessage) }
+  else{
+      req.body.name = name.trim();
+      next();
+  }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,55 +1,61 @@
-const router = require('express').Router()
-const Account = require('./accounts-model')
+const router = require("express").Router();
+const Account = require("./accounts-model");
 
-const { 
+const {
   checkAccountId,
   checkAccountPayload,
-  checkAccountNameUnique
-} = require('./accounts-middleware')
+  checkAccountNameUnique,
+} = require("./accounts-middleware");
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const accounts = await Account.getAll()
-    res.json(accounts)
+    const accounts = await Account.getAll();
+    res.json(accounts);
   } catch (err) {
-    next(err)
-  }
-})
-
-router.get('/:id', checkAccountId, (req, res, next) => {
-  res.json(req.account)
-  next()
-})
-
-router.post('/',checkAccountPayload, async (req, res, next) => {
-  try {
-    const account = await Account.create(req.body)
-    res.status(201).json(account)
-  } catch (err) {
-    next(err)
-  }
-})
-
-router.put('/:id', (req, res, next) => {
-  try {
-    res.json('hello')
-  } catch (err) {
-    next(err)
+    next(err);
   }
 });
 
-router.delete('/:id', (req, res, next) => {
-  try {
-    res.json('hello')
-  } catch (err) {
-    next(err)
+router.get("/:id", checkAccountId, (req, res, next) => {
+  res.json(req.account);
+  next();
+});
+
+router.post('/', checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
+  try{
+      const newAccount = await Account.create(req.body)
+      res.status(201).json(newAccount)
   }
+  catch(err){next(err)}
 })
+
+router.put(
+  "/:id",
+  checkAccountId,
+  checkAccountPayload,
+  async (req, res, next) => {
+    try {
+      const updated = await Account.updateById(req.params.id, req.body);
+      res.json(updated);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete("/:id", checkAccountId, async (req, res, next) => {
+  try {
+    await Account.deleteById(req.params.id);
+    res.json(req.account);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
-  })
-})
+  });
+});
 
 module.exports = router;
